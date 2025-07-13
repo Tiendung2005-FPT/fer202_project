94ab62f686ee3a95c55fe3f9e990c04ec91e465c
hoangpzo <filmmusic249@gmail.com>
2025-07-14 01:03:17 +0700

    form login, register

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,10 +1,28 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
     return(
         <Container>
             <Row>
+                <h3 className="text-center mt-2">Đăng nhập</h3>
                 <Col>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Nhập email..."></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Mật khẩu</Form.Label>
+                            <Form.Control type="password" placeholder="Nhập mật khẩu..."></Form.Control>
+                        </Form.Group>
+
+                        <Button className="mt-2" type="submit">
+                            Đăng nhập
+                        </Button>
+                    </Form>
+                    Chưa có tài khoản <Link to={`/register`}>Tạo tài khoản</Link>
                 </Col>
             </Row>
         </Container>
--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,10 +1,39 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Register() {
-    return (
+    return(
         <Container>
             <Row>
+                <h3 className="text-center mt-2">Tạo tài khoản</h3>
                 <Col>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Họ và tên</Form.Label>
+                            <Form.Control type="text" placeholder="Nhập họ và tên..."></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Nhập email..."></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Mật khẩu</Form.Label>
+                            <Form.Control type="password" placeholder="Nhập mật khẩu..."></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Xác nhận mật khẩu</Form.Label>
+                            <Form.Control type="password" placeholder="Nhập lại mật khẩu..."></Form.Control>
+                        </Form.Group>
+
+                        <Button className="mt-2" type="submit">
+                            Tạo tài khoản
+                        </Button>
+                    </Form>
+
+                    Đã có tài khoản <Link to={`/login`}>Đăng nhập</Link>
                 </Col>
             </Row>
         </Container>
