f72aa6d66c69a1e39cd8a151462389647f3739a6
hoangpzo <filmmusic249@gmail.com>
2025-07-14 00:44:51 +0700

    update form auth

---
+++ src/Components/Auth/Login.js
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Login() {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
---
+++ src/Components/Auth/Register.js
@@ -0,0 +1,12 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Register() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
