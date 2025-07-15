af7951019943ed9535806823d5e3d18a6f991aef
hoangpzo <filmmusic249@gmail.com>
2025-07-16 00:53:19 +0700

    update register, forgot

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import UserDetail from './users/userDetail';
 import FilterStories from './stories/filterStories';
 import StoryPage from "./components/storyPage/index";
 import ReadStory from "./components/readStorys/index";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import ForgotPassword from "./components/Auth/ForgotPassword";
 import ChapterWriter from "./components/ChapterWrite/ChapterWriter.js";
 import ChapterEdit from "./components/ChapterWrite/ChapterEdit.js";
 import Canvas from "./components/ChapterWrite/Canvas.js";
@@ -24,6 +27,9 @@ function App() {
         <Route path="/storypage/:id" element={<StoryPage />} />
         <Route path="/readStory/:storyId/:chapterId" element={<ReadStory />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/register" element={<Register></Register>} />
+        <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,16 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function ForgotPassword() {
+    const [email, setEmail] = useState('')
+
+    const handleReset = (e) => {
+        e.preventDefault();
+
+        axios.get('http://localhost:9999/users')
+            .then(result => {
+                const userList = result.data
+                const checkEmail = userList.find(e => e.email === email)
+                if(!checkEmail) {
+                    alert('Tài khoản Email không tồn tại trong hệ thống')
+                    return
+                }
+
+
+            })
+            .catch(err => console.error(err))
+    }
+
     return (
         <Container>
             <Row>
-                <h3>Quên mật khẩu</h3>
+                <h3 className="text-center mt-2">Quên mật khẩu</h3>
                 <Col>
-                    <Form>
+                    <Form onSubmit={handleReset}>
                         <Form.Group>
                             <Form.Label>Email *</Form.Label>
-                            <Form.Control type="email" placeholder="Nhập email..." required></Form.Control>
+                            <Form.Control type="email" placeholder="Nhập email..." onChange={(e) => setEmail(e.target.value)} required></Form.Control>
                         </Form.Group>
 
                         <Button className="mt-3" type="submit">Gửi</Button>
